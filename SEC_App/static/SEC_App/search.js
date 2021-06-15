f6e489e0bcf7f99fdcf009a12d34ca23b9353ba7
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-06-15 11:00:16 +0300

    Update search.js

--- SEC_App/static/SEC_App/search.js
+++ SEC_App/static/SEC_App/search.js
@@ -5,3 +5,24 @@ Date.prototype.toDateInputValue = (function() {
 });
 
 document.getElementById('end_date').value = new Date().toDateInputValue();
+
+function keywords(){
+    var text = document.getElementById('keyword').value;
+    var text_list = text.split(' ');
+
+    if (text_list.length>1){
+        var or_and = document.getElementById('or_and');
+        or_and.classList.add('show');
+    }
+    else {
+        or_and.classList.remove('show');
+    }
+
+    if (text_list.length>1){
+        var keyword_option = document.getElementById('keyword_option');
+        keyword_option.classList.add('show');
+    }
+    else {
+        keyword_option.classList.remove('show');
+    }
+}

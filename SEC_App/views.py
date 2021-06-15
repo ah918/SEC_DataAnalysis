c864c7fba8d2190e4bbe9e83550453408debeea9
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-06-15 12:41:55 +0300

    ..

--- SEC_App/views.py
+++ SEC_App/views.py
@@ -1,5 +1,7 @@
+from django.db.models.fields import NullBooleanField
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Request
 
 # Create your views here.
 
@@ -8,4 +10,19 @@ def search(request):
 
 def analysis(request):
     keyword = request.POST['keyword']
+    period_start = request.POST.get('start_date', None) #"{{placement.date|date:'Y-m-d' }}"
+    period_end = request.POST.get('end_date', None) #request.POST['end_date']
+    time_start = request.POST.get('strat_time', None)
+    time_end = request.POST.get('end_time', None)
+    rangeOfsearch = int(request.POST.get('domain', '0'))
+    if request.POST['or_and']== 'and':
+        includeAll = True
+    else:
+        includeAll = False
+
+    print(period_start)
+    print(rangeOfsearch)
+    req = Request(keyword=keyword, period_start=period_start, period_end=period_end, time_start=time_start, time_end=time_end, rangeOfsearch=0, includeAll=includeAll)
+    req.save()
+
     return HttpResponse(keyword)
